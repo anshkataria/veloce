3459263980f3d85722292e26894b7cadc6b27284
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-07-07 16:20:39 +1000

    make test independent of local postgres

--- backend/api/src/test/java/com/veloce/api/ApiApplicationTests.java
+++ backend/api/src/test/java/com/veloce/api/ApiApplicationTests.java
@@ -1,10 +1,28 @@
 package com.veloce.api;
 
+import com.veloce.api.repository.CarRepository;
+import com.veloce.api.repository.OrderRepository;
+import com.veloce.api.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=" +
+				"org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration," +
+				"org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration," +
+				"org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
+})
 class ApiApplicationTests {
+
+	@MockitoBean
+	private UserRepository userRepository;
+
+	@MockitoBean
+	private CarRepository carRepository;
+
+	@MockitoBean
+	private OrderRepository orderRepository;
 
 	@Test
 	void contextLoads() {
